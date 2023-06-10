9540fa66821365f2fb7b437308a6a0f3e624ff89
ivanconde <iconde@isesoft.co>
2023-06-09 22:45:00 -0400

    comment removed

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 # This is a sample Python script.
-# This class will be renamed to app.py
 
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
