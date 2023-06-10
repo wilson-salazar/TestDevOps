346e94e81b1135f77be6107e7ff06ef9287adb69
wilson.salazar <wsalazar@omnixcorp.com>
2023-06-09 20:35:02 -0500

    Primer commit, clase negocio y test

---
+++ negocio/Calculadora.py
@@ -0,0 +1,10 @@
+class Calculadora:
+
+    def suma(self, num1, num2):
+        return num1 + num2
+
+    def resta(self, num1, num2):
+        return num1 - num2
+
+    def multiplicar(self, num1, num2):
+        return num1 * num2
---
+++ test_general/test_calculadora.py
@@ -0,0 +1,24 @@
+import pytest
+
+from negocio import Calculadora
+
+class TestLaboratorio:
+    calculadora = Calculadora()
+
+    @pytest.mark.wilo
+    def test_sumar(self):
+        print("[Funcion sumar]")
+        assert self.calculadora.suma(3, 2) == 5
+        assert self.calculadora.suma(8, 2) == 10
+
+    @pytest.mark.wilo
+    def test_restar(self):
+        print("[Funcion restar]")
+        assert self.calculadora.resta(5, 3) == 5
+        assert self.calculadora.resta(14, 4) == 10
+
+    @pytest.mark.regression
+    def test_multiplicar(self):
+        print("[Funcion multiplicar]")
+        assert self.calculadora.multiplicar(5, 3) == 15
+        assert self.calculadora.multiplicar(10, 2) == 20
